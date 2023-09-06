1c8c4d3eb20daa4a356efe6413a305b9da7aab9d
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2023-09-06 22:08:58 +0500

    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

--- components/TheAbout.vue
+++ components/TheAbout.vue
@@ -1,24 +1,56 @@
 <template>
-  <div class="about">
-    <img src="@/assets/img/about-icon_1.svg" alt="" class="absolute icon-left-1" />
-    <img src="@/assets/img/about-icon_2.svg" alt="" class="absolute icon-left-2" />
-    <img src="@/assets/img/about-icon_1.svg" alt="" class="absolute icon-left-3" />
-    <img src="@/assets/img/about-icon_2.svg" alt="" class="absolute icon-left-4" />
-    <img src="@/assets/img/about-icon_2.svg" alt="" class="absolute icon-left-5" />
-    <img src="@/assets/img/about-icon_1.svg" alt="" class="absolute icon-left-6" />
-    <img src="@/assets/img/about-icon_2.svg" alt="" class="absolute icon-left-7" />
+  <div class="about" ref="about">
+    <img
+      src="@/assets/img/about-icon_1.svg"
+      alt=""
+      class="absolute icon-left-1"
+    />
+    <img
+      src="@/assets/img/about-icon_2.svg"
+      alt=""
+      class="absolute icon-left-2"
+    />
+    <img
+      src="@/assets/img/about-icon_1.svg"
+      alt=""
+      class="absolute icon-left-3"
+    />
+    <img
+      src="@/assets/img/about-icon_2.svg"
+      alt=""
+      class="absolute icon-left-4"
+    />
+    <img
+      src="@/assets/img/about-icon_2.svg"
+      alt=""
+      class="absolute icon-left-5"
+    />
+    <img
+      src="@/assets/img/about-icon_1.svg"
+      alt=""
+      class="absolute icon-left-6"
+    />
+    <img
+      src="@/assets/img/about-icon_2.svg"
+      alt=""
+      class="absolute icon-left-7"
+    />
 
     <div class="about__container">
       <div class="about__row">
-        <div class="about__image" data-aos="zoom-in">
+        <div class="about__image" ref="image">
           <img src="@/assets/img/about.svg" alt="" />
         </div>
         <div class="about__content">
-          <h1>{{ $t('about') }}</h1>
+          <h1>{{ $t("about") }}</h1>
           <p v-if="aboutUs">
             {{ translateText(aboutUs) }}
           </p>
-          <base-button banner @clickedButton="$router.push(localeLocation('/about'))">{{ $t('readMore') }}</base-button>
+          <base-button
+            banner
+            @clickedButton="$router.push(localeLocation('/about'))"
+            >{{ $t("readMore") }}</base-button
+          >
         </div>
       </div>
     </div>
@@ -26,154 +58,179 @@
 </template>
 
 <script>
-  import translate from '@/mixins/translate'
-  export default {
-    props: {
-      aboutUs: {
-        type: Object,
-        default: () => null
-      }
+import translate from "@/mixins/translate";
+export default {
+  props: {
+    aboutUs: {
+      type: Object,
+      default: () => null,
     },
-    mixins: [translate]
-  }
+  },
+  mixins: [translate],
+  data() {
+    return {
+      observer: null,
+    };
+  },
+  mounted() {
+    if (this.$refs.about) {
+      const options =
+        {
+          rootMargin: "1000px 0px 0px 0px",
+          threshold: 1.0,
+        } || {};
+      this.observer = new IntersectionObserver(async ([entry]) => {
+        if (entry && entry.isIntersecting) {
+          console.log(">>>", this.$refs.image);
+          this.$refs.image.classList.add("aos");
+        }
+      }, options);
+      this.observer.observe(this.$refs.about);
+    }
+  },
+  destroyed() {
+    this.observer.disconnect();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .about {
-    position: relative;
-    padding: 40px 0;
-    background-color: var(--background);
-    margin-bottom: 70px;
-    @media (max-width: 767px) {
-      margin-bottom: 40px;
+.about {
+  position: relative;
+  padding: 40px 0;
+  background-color: var(--background);
+  margin-bottom: 70px;
+  @media (max-width: 767px) {
+    margin-bottom: 40px;
+  }
+  &:hover {
+    .absolute {
+      transform: scale(1.1);
     }
-    &:hover {
-      .absolute {
-        transform: scale(1.1);
+    .about__image {
+      img {
+        transform: scale(1.05);
       }
-      .about__image {
-        img {
-          transform: scale(1.05);
-        }
-      }
     }
-    @media (max-width: 767px) {
-      padding: 20px 0;
-    }
-    &__container {
-    }
+  }
+  @media (max-width: 767px) {
+    padding: 20px 0;
+  }
+  &__container {
+  }
 
-    &__row {
-      display: grid;
-      align-items: center;
-      grid-template-columns: minmax(400px, 560px) 1fr;
-      gap: 40px;
-      @media (max-width: 767px) {
-        grid-template-columns: 1fr;
-        gap: 30px;
-      }
+  &__row {
+    display: grid;
+    align-items: center;
+    grid-template-columns: minmax(400px, 560px) 1fr;
+    gap: 40px;
+    @media (max-width: 767px) {
+      grid-template-columns: 1fr;
+      gap: 30px;
     }
+  }
 
-    &__image {
+  &__image {
+    transform: translateY(-100px);
+    transition: 0.2s all;
+    opacity: 0;
+    &.aos {
+      opacity: 1;
       transform: translateY(-100px);
       transition: 0.2s all;
-      opacity: 0;
-      &.aos {
-        opacity: 1;
-        transform: translateY(-80px);
-      }
-      img {
-        width: 100%;
-        height: 100%;
-        transition: all 0.3s ease 0s;
-      }
-      @media (max-width: 767px) {
-        order: 2;
-        transform: translateY(0);
-      }
     }
-
-    &__content {
-      h1 {
-        color: var(--text);
-        font-size: 60px;
-        font-weight: 900;
-        line-height: calc(65 / 60) * 100%;
-        margin-bottom: 30px;
-      }
-
-      p {
-        color: var(--text);
-        font-size: 16px;
-        font-weight: 400;
-        line-height: 160%;
-        letter-spacing: 0.32px;
-        margin-bottom: 30px;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-line-clamp: 4;
-        -webkit-box-orient: vertical;
-      }
-      @media (max-width: 767px) {
-        order: 1;
-        h1 {
-          font-size: 30px;
-          margin-bottom: 14px;
-        }
-        p {
-          color: #000;
-          margin-bottom: 14px;
-        }
-      }
+    img {
+      width: 100%;
+      height: 100%;
+      transition: all 0.3s ease 0s;
     }
-  }
-  .absolute {
-    position: absolute;
-    z-index: 10;
-    transition: all 0.3s ease 0s;
     @media (max-width: 767px) {
-      display: none;
+      order: 2;
+      transform: translateY(0);
     }
   }
-  .icon-left-1 {
-    top: 5%;
-    left: 0%;
-    width: 130px;
-    height: 130px;
+
+  &__content {
+    h1 {
+      color: var(--text);
+      font-size: 60px;
+      font-weight: 900;
+      line-height: calc(65 / 60) * 100%;
+      margin-bottom: 30px;
+    }
+
+    p {
+      color: var(--text);
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 160%;
+      letter-spacing: 0.32px;
+      margin-bottom: 30px;
+      overflow: hidden;
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+    }
+    @media (max-width: 767px) {
+      order: 1;
+      h1 {
+        font-size: 30px;
+        margin-bottom: 14px;
+      }
+      p {
+        color: #000;
+        margin-bottom: 14px;
+      }
+    }
   }
-  .icon-left-2 {
-    top: 25%;
-    left: 4%;
-    width: 80px;
-    height: 80px;
+}
+.absolute {
+  position: absolute;
+  z-index: 10;
+  transition: all 0.3s ease 0s;
+  @media (max-width: 767px) {
+    display: none;
   }
-  .icon-left-3 {
-    top: 35%;
-    width: 120px;
-    height: 120px;
-  }
-  .icon-left-4 {
-    top: 50%;
-    left: 2.6%;
-    width: 80px;
-    height: 80px;
-  }
-  .icon-left-5 {
-    top: 53%;
-    left: 6%;
-    width: 180px;
-    height: 180px;
-  }
-  .icon-left-6 {
-    top: 68%;
-    left: 4%;
-    width: 110px;
-    height: 110px;
-  }
-  .icon-left-7 {
-    top: 75%;
-    left: 6.6%;
-    width: 180px;
-    height: 180px;
-  }
+}
+.icon-left-1 {
+  top: 5%;
+  left: 0%;
+  width: 130px;
+  height: 130px;
+}
+.icon-left-2 {
+  top: 25%;
+  left: 4%;
+  width: 80px;
+  height: 80px;
+}
+.icon-left-3 {
+  top: 35%;
+  width: 120px;
+  height: 120px;
+}
+.icon-left-4 {
+  top: 50%;
+  left: 2.6%;
+  width: 80px;
+  height: 80px;
+}
+.icon-left-5 {
+  top: 53%;
+  left: 6%;
+  width: 180px;
+  height: 180px;
+}
+.icon-left-6 {
+  top: 68%;
+  left: 4%;
+  width: 110px;
+  height: 110px;
+}
+.icon-left-7 {
+  top: 75%;
+  left: 6.6%;
+  width: 180px;
+  height: 180px;
+}
 </style>
